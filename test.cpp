c8f552fa8918821638043a80c0b905afb6dcf491
caoboxi <541190157@qq.com>
2022-06-14 10:00:43 +0000

    Description: 链表

--- test.cpp
+++ test.cpp
@@ -1,11 +1,16 @@
 #include "test.h"
 
-int main() {
-    vector<int> num {-1,-2,-3,-4,-5};
-    vector<int> res(2);
+int main(int argc,char **argv) {
+    // vector<int> num {-1,-2,-3,-4,-5};
+    // vector<int> res(2);
     Solution test;
-    res = test.twoSum(num,-8);
-    cout << res[0] << " " << res[1];
+    // res = test.twoSum(num,-8);
+    // cout << res[0] << " " << res[1];
 
+
+
+    test.create_lde();
+
+    // test.del_lde();
     return 0;
 }
--- test.h
+++ test.h
@@ -13,7 +13,21 @@ struct ListNode {
   };
   
 class Solution {
+
+private:
+    ListNode * lde1,* lde2;
+    ListNode * lde1_head, * lde2_head;
+    ListNode * temp1, * temp2;
 public:
+    Solution() {
+        lde1 = NULL;
+        lde2 = NULL;
+        lde1_head = NULL;
+        lde2_head = NULL;
+        temp1 = NULL;
+        temp2 = NULL;
+    }
+    ~Solution() { del_lde(); }
     vector<int> twoSum(vector<int>& nums, int target) {
         int x,y;
         for(x = 0;x<nums.size()-1;x++) {
@@ -25,19 +39,67 @@ public:
         }
         return {x,y};
     }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* addTwoNumbers() {
         stack<ListNode> list1,list2;
-        while(l1 != NULL) {
-            list1.push(*l1);
-            l1 = l1->next;
+        temp1 = lde1_head;
+        temp2 = lde2_head;
+        while(temp1 != NULL) {
+            list1.push(*temp1);
+            temp1 = temp1->next;
         }
         while(list1.size() >0) {
-            cout << list1.top();
+            cout << list1.top().val;
             list1.pop();
         }
-        while(l2 != NULL) {
-            list2.push(*l2);
-            l2 = l2->next;
+        while(temp2 != NULL) {
+            list2.push(*temp2);
+            temp2 = temp2->next;
+        }
+        while(list1.size() >0) {
+            cout << list1.top().val;
+            list1.pop();
+        }
+    }
+    void create_lde() {
+        for(int x = 0;x< 5;x++) {
+            lde1 = new ListNode;
+            lde2 = new ListNode;
+            lde1->val = x+1;
+            lde2->val = x+2;
+            
+            if(lde1_head == NULL && lde2_head == NULL) {
+                lde1_head = lde1;
+                lde2_head = lde2;
+            }
+            else if (temp1 != NULL && temp2 != NULL){
+                temp1->next = lde1;
+                temp2->next = lde2;
+            }
+            temp1 = lde1;
+            temp2 = lde2;
+            temp1->next = NULL;
+            temp2->next = NULL;
+        }
+    }
+    void del_lde() {
+        temp1 = lde1_head;
+        temp2 = lde2_head;
+        while(temp2 != NULL) {
+            cout << temp2->val;
+            temp2 = temp1->next;
+            delete temp1;
+            temp1 = NULL;
+            temp1 = temp2;
+        }
+        cout << endl;
+        temp1 = lde2_head;
+        temp2 = lde2_head;
+        while(temp2 != NULL) {
+            cout << temp1->val;
+            temp2 = temp1->next;
+            delete temp1;
+            temp1 = NULL;
+            temp1 = temp2;
         }
     }
 };
